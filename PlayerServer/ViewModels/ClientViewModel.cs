561b0435c9114c62d9722ad9d436842a43c1bb99
CloverGit <CloverGit@hotmail.com>
2024-11-04 16:26:31 +0800

    :recycle: refactor: use command binding for copy in ClientView

--- PlayerServer/ViewModels/ClientViewModel.cs
+++ PlayerServer/ViewModels/ClientViewModel.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Globalization;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
 using PlayerServer.Models;
+using PlayerServer.Utilities;
 
 namespace PlayerServer.ViewModels;
 
@@ -11,6 +14,7 @@ public class ClientViewModel : ViewModelBase
     public ClientViewModel()
     {
         ClientListUpdated += UpdateClients;
+        CopyCommand = new RelayCommand<object>(CopyToClipboard);
     }
 
     public string? Header { get; set; }
@@ -20,6 +24,7 @@ public class ClientViewModel : ViewModelBase
     public string? ConnectionTime { get; set; }
 
     public ObservableCollection<ClientViewModel> Clients { get; set; } = [];
+    public ICommand CopyCommand { get; }
 
     public static event Action<List<Client>>? ClientListUpdated;
 
@@ -41,5 +46,15 @@ public class ClientViewModel : ViewModelBase
     public static void OnClientListUpdated(List<Client> clients)
     {
         ClientListUpdated?.Invoke(clients);
+    }
+
+    private void CopyToClipboard(object? parameter)
+    {
+        if (parameter is not string propertyName) return;
+        // 使用反射获取绑定的属性值
+        var property = GetType().GetProperty(propertyName);
+        var value = property?.GetValue(this) as string;
+        if (!string.IsNullOrEmpty(value))
+            _ = Clipboard.SetText(value);
     }
 }
--- PlayerServer/Views/ClientView.axaml.cs
+++ PlayerServer/Views/ClientView.axaml.cs
@@ -1,7 +1,4 @@
 using Avalonia.Controls;
-using Avalonia.Interactivity;
-using PlayerServer.Utilities;
-using PlayerServer.ViewModels;
 
 namespace PlayerServer.Views;
 
@@ -11,15 +8,5 @@ public partial class ClientView : UserControl
     {
         InitializeComponent();
         // DataContext = new ClientViewModel();
-    }
-
-    private async void CopyButton_Click(object? sender, RoutedEventArgs args)
-    {
-        if (sender is not Button { DataContext: ClientViewModel client } button) return;
-        if (button.CommandParameter is not string propertyName) return;
-        var propertyInfo = client.GetType().GetProperty(propertyName);
-        var value = propertyInfo?.GetValue(client) as string ?? string.Empty;
-
-        await Clipboard.SetText(value);
     }
 }
